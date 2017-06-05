a9380c87fed73997fd8993629f3b5f24d76cb64c
NAT <nat@magicnat.com>
2017-06-06 03:08:02 +0800

    init.

---
+++ index.js
@@ -0,0 +1,71 @@
+var fs = require('fs'), util = require('util'), stream = require('stream'), es = require('event-stream');
+var args = process.argv.slice(2);
+
+var parse_route = function (infile, outfile) {
+  var ln = 0;
+  var routes = [];
+  var isFieldHead = line => /^(\d{4})-/.test(line);
+  var robj = {};
+
+  var read_stream = fs.createReadStream(infile).pipe(es.split()).pipe(es.mapSync(line => {
+    read_stream.pause();
+    ln++;
+    if (ln % 100000 == 0) console.log('line: ', ln);
+    line = line.replace(/\s+/g, ' ');
+    if (isFieldHead(line)) {
+      var fid = Number.parseInt(line.substring(0, 4));
+      var rline = line.substring(5);
+      switch (fid) {
+        case 1007:
+          var rinfo = rline.split(' ');
+          if (robj.route) routes.push(robj);
+          robj = {};
+          robj.route = rinfo[0];
+          robj.via = rinfo[2];
+          robj.dev = rinfo[4];
+          robj.peer = rinfo[5].substring(1);
+          robj.uptime = rinfo[6].slice(0, -1);
+          break;
+        case 1008:
+          robj.types = rline.split(' ').slice(2);
+          break;
+        case 1012:
+          robj.origin = rline.split(' ')[2];
+      }
+    } else {
+      var rbase = line.split(' ').slice(1);
+      switch (rbase[0]) {
+        case 'BGP.as_path:':
+          robj.as_path = rbase.slice(1).map(n => Number.parseInt(n));
+          break;
+        case 'BGP.next_hop:':
+          robj.next_hop = rbase.slice(1);
+          break;
+        case 'BGP.local_pref':
+          robj.local_pref = Number.parseInt(rbase.slice(1)[0]);
+          break;
+        case 'BGP.community:':
+          robj.bgp_community = rbase.slice(1).map(s => s.replace(/[\(\)]/g, '').replace(',', ':'));
+          break;
+        case 'BGP.aggregator:':
+          var aggr_ = rbase.slice(1);
+          var aggr = {};
+          aggr.ip = aggr_[0];
+          aggr.asn = Number.parseInt(aggr_[1].replace('AS', ''));
+          robj.aggregator = aggr;
+      }
+    }
+    read_stream.resume();
+  }));
+
+  return new Promise((res, rej) => {
+    read_stream.on('error', err => res(routes)); // reslove anyway.
+    read_stream.on('end', () => res(routes));
+  });
+};
+
+parse_route(args[0]).then(routes => {
+  var ws = fs.createWriteStream(args[1]);
+  ws.write(JSON.stringify(routes));
+  ws.end();
+});
---
+++ readme.md
@@ -0,0 +1,20 @@
+node-bird-routeparse
+---
+
+Generate JSON from bird's stdout, if you nat-lab/node-bird-routedump use too much of memory, try this one.
+
+### Todo
+
+- Support recursive route
+- Support multipath
+
+### Usage
+
+```
+root@router $ bird -v 'show route all' > routes.txt
+root@router $ node nbrp.js routes.txt routes.json
+```
+
+### Licenses
+
+MIT
